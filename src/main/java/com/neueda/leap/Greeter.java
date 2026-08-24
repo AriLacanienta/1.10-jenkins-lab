5e373413165ba4f66a89982050a18a6923c749d9
james. <james@james.com>
2026-08-24 20:36:15 +0000

    texas-ification

--- src/main/java/com/neueda/leap/Greeter.java
+++ src/main/java/com/neueda/leap/Greeter.java
@@ -4,7 +4,7 @@ import java.io.Console;
 
 public class Greeter {
     public String greet(String name) {
-        return "Good day, " + name;
+        return "Hopwdy, " + name;
     }
 
     public void doNothing(int num){
