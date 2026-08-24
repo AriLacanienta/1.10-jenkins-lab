b981c65f000cae8afb8d046df61dd29400f2f544
james. <james@james.com>
2026-08-24 20:31:02 +0000

    added empty function

--- src/main/java/com/neueda/leap/Greeter.java
+++ src/main/java/com/neueda/leap/Greeter.java
@@ -1,7 +1,13 @@
 package com.neueda.leap;
 
+import java.io.Console;
+
 public class Greeter {
     public String greet(String name) {
         return "Good day, " + name;
+    }
+
+    public void doNothing(int num){
+        //noop
     }
 }
